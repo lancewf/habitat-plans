72b736b9171b34c01b7c8faa93bd53e045470148
Lance Finfrock <lfinfrock@chef.io>
2018-03-14 06:43:53 -0700

    Adding a php-fpm plan
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

---
+++ php-fpm/habitat/plan.sh
@@ -0,0 +1,31 @@
+# This file is the heart of your application's habitat.
+# See full docs at https://www.habitat.sh/docs/reference/plan-syntax/
+pkg_repo=lemp
+pkg_name=php-fpm
+pkg_origin=lancewf
+pkg_version="0.2.0"
+pkg_maintainer="Lance Finfrock <lfinfrock@chef.io>"
+pkg_license=('Apache-2.0')
+pkg_source=""
+
+pkg_svc_user=root
+pkg_svc_group=$pkg_svc_user
+
+pkg_svc_run="php-fpm -y $pkg_svc_config_path/php-fpm.conf"
+
+pkg_deps=(core/php)
+
+pkg_exports=(
+  [port]=port
+  [bind]=bind
+)
+
+pkg_binds=()
+
+do_build(){
+  return 0
+}
+
+do_install() {
+  return 0
+}
