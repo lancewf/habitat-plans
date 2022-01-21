e7d67a8dfff82af7199f31205dd89accb1e71775
Lance Finfrock <lancewf@gmail.com>
2022-01-20 18:56:07 -0800

    adding php
    Signed-off-by: Lance Finfrock <lancewf@gmail.com>

---
+++ php/plan.sh
@@ -0,0 +1,65 @@
+pkg_name=php
+pkg_distname=php
+pkg_origin=core
+pkg_version=7.1.4
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('PHP-3.01')
+pkg_upstream_url=http://php.net/
+pkg_description="PHP is a popular general-purpose scripting language that is especially suited to web development."
+pkg_source=https://php.net/get/${pkg_distname}-${pkg_version}.tar.bz2/from/this/mirror
+pkg_filename=${pkg_distname}-${pkg_version}.tar.bz2
+pkg_dirname=${pkg_distname}-${pkg_version}
+pkg_shasum=39bf697836e2760b3a44ea322e9e5f1f5b1f07abeb0111f6495eff7538e25805
+pkg_deps=(
+  core/coreutils
+  core/curl
+  core/glibc
+  core/libxml2
+  core/libjpeg-turbo
+  core/libpng
+  core/openssl
+  core/readline
+  core/zlib
+)
+pkg_build_deps=(
+  core/bison2
+  core/gcc
+  core/make
+  core/re2c
+)
+pkg_bin_dirs=(bin sbin)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+pkg_interpreters=(bin/php)
+pkg_exports=(
+  [port]=port
+  [local_only]=local_only
+)
+pkg_svc_run="php-fpm --fpm-config ${pkg_svc_config_path}/php-fpm.conf -c ${pkg_svc_config_path}"
+
+do_build() {
+  ./configure --prefix="$pkg_prefix" \
+    --enable-exif \
+    --enable-fpm \
+    --with-fpm-user=hab \
+    --with-fpm-group=hab \
+    --enable-mbstring \
+    --enable-opcache \
+    --with-mysql=mysqlnd \
+    --with-mysqli=mysqlnd \
+    --with-pdo-mysql=mysqlnd \
+    --with-readline="$(pkg_path_for readline)" \
+    --with-curl="$(pkg_path_for curl)" \
+    --with-gd \
+    --with-jpeg-dir="$(pkg_path_for libjpeg-turbo)" \
+    --with-libxml-dir="$(pkg_path_for libxml2)" \
+    --with-openssl="$(pkg_path_for openssl)" \
+    --with-png-dir="$(pkg_path_for libpng)" \
+    --with-xmlrpc \
+    --with-zlib="$(pkg_path_for zlib)"
+  make
+}
+
+do_check() {
+  make test
+}
