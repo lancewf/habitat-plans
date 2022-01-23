8243688854aca97374011c2ac3b5aef09e00a335
Lance Finfrock <lancewf@gmail.com>
2022-01-22 16:35:27 -0800

    Adding php plan
    Signed-off-by: Lance Finfrock <lancewf@gmail.com>

--- php/plan.sh
+++ php/plan.sh
@@ -1,29 +1,38 @@
 pkg_name=php
 pkg_distname=php
 pkg_origin=core
-pkg_version=7.1.4
+pkg_version=7.4.16
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('PHP-3.01')
 pkg_upstream_url=http://php.net/
 pkg_description="PHP is a popular general-purpose scripting language that is especially suited to web development."
-pkg_source=https://php.net/get/${pkg_distname}-${pkg_version}.tar.bz2/from/this/mirror
-pkg_filename=${pkg_distname}-${pkg_version}.tar.bz2
+pkg_source=https://php.net/get/${pkg_distname}-${pkg_version}.tar.xz/from/this/mirror
+pkg_filename=${pkg_distname}-${pkg_version}.tar.xz
 pkg_dirname=${pkg_distname}-${pkg_version}
-pkg_shasum=39bf697836e2760b3a44ea322e9e5f1f5b1f07abeb0111f6495eff7538e25805
+pkg_shasum=1c16cefaf88ded4c92eed6a8a41eb682bb2ef42429deb55f1c4ba159053fb98b
 pkg_deps=(
+  core/bzip2
   core/coreutils
   core/curl
   core/glibc
-  core/libxml2
+  core/icu
   core/libjpeg-turbo
   core/libpng
+  core/libxml2
+  core/libzip
   core/openssl
   core/readline
+  core/zip
   core/zlib
+  core/gcc-libs
+  core/oniguruma
+  core/sqlite
 )
 pkg_build_deps=(
-  core/bison2
+  core/autoconf
+  core/bison
   core/gcc
+  core/libgd
   core/make
   core/re2c
 )
@@ -38,26 +47,30 @@ pkg_exports=(
 pkg_svc_run="php-fpm --fpm-config ${pkg_svc_config_path}/php-fpm.conf -c ${pkg_svc_config_path}"
 
 do_build() {
-  ./configure --prefix="$pkg_prefix" \
-    --enable-exif \
+  ./configure --prefix="${pkg_prefix}" \
     --enable-fpm \
     --with-fpm-user=hab \
     --with-fpm-group=hab \
-    --enable-mbstring \
-    --enable-opcache \
-    --with-mysql=mysqlnd \
     --with-mysqli=mysqlnd \
     --with-pdo-mysql=mysqlnd \
     --with-readline="$(pkg_path_for readline)" \
-    --with-curl="$(pkg_path_for curl)" \
-    --with-gd \
-    --with-jpeg-dir="$(pkg_path_for libjpeg-turbo)" \
-    --with-libxml-dir="$(pkg_path_for libxml2)" \
-    --with-openssl="$(pkg_path_for openssl)" \
-    --with-png-dir="$(pkg_path_for libpng)" \
+    --with-curl \
+    --with-jpeg \
+    --with-libxml \
+    --with-openssl \
     --with-xmlrpc \
-    --with-zlib="$(pkg_path_for zlib)"
-  make
+    --with-zip \
+    --with-zlib \
+    --with-bz2="$(pkg_path_for bzip2)" \
+    --with-gettext="$(pkg_path_for glibc)" \
+    --enable-bcmath \
+    --enable-exif \
+    --enable-mbstring \
+    --enable-opcache \
+    --enable-gd \
+    --enable-intl
+
+  make -j "$(nproc)"
 }
 
 do_check() {
