761dd34cc8d90437c5735af87a162270617e7759
Lance Finfrock <lancewf@gmail.com>
2022-01-23 16:10:15 -0800

    Creating a composite php-fpm plan
    Signed-off-by: Lance Finfrock <lancewf@gmail.com>

--- php-fpm/plan.sh
+++ php-fpm/plan.sh
@@ -1,27 +1,21 @@
 # This file is the heart of your application's habitat.
 # See full docs at https://www.habitat.sh/docs/reference/plan-syntax/
-pkg_repo=lemp
 pkg_name=php-fpm
 pkg_origin=lancewf
-pkg_version="0.2.0"
-pkg_maintainer="Lance Finfrock <lfinfrock@chef.io>"
+pkg_version="7.4.16"
+pkg_maintainer="Lance Finfrock <lancewf@gmail.com>"
 pkg_license=('Apache-2.0')
 pkg_source=""
 pkg_upstream_url="https://github.com/lancewf/habitat-plans"
 
-pkg_svc_user=root
-pkg_svc_group=$pkg_svc_user
+pkg_deps=(core/php/7.4.16/20211019171114)
 
-pkg_svc_run="php-fpm -y $pkg_svc_config_path/php-fpm.conf"
-
-pkg_deps=(core/php core/readline)
+pkg_svc_run="php-fpm --fpm-config ${pkg_svc_config_path}/php-fpm.conf -c ${pkg_svc_config_path}"
 
 pkg_exports=(
   [port]=port
-  [bind]=bind
+  [local_only]=local_only
 )
-
-pkg_binds=()
 
 do_build(){
   return 0
